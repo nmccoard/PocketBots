64665e08c849485bad5705198623755e9b456e92
Brian Earl <yodaking_2003@hotmail.com>
2020-07-18 10:58:27 -0600

    fixed the hit animations for the non-blob monsters

--- app/src/main/java/com/example/pocketbots/EndingActivity.java
+++ app/src/main/java/com/example/pocketbots/EndingActivity.java
@@ -15,37 +15,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndingActivity extends AppCompatActivity {
+
     public Button mainMenu;
+
     public SharedPreferences gameSettings;
     public SharedPreferences.Editor editGame;
-    public int level;
 
+    public int level;
     public int height;
     public int width;
 
     public TextView congratView;
+
     public ImageView logoView;
-
     public ImageView bgImageView;
-
     public ImageView boyImageView;
-    //public ImageView boyImageView2;
-    //public ImageView boyImageView3;
-    public AnimationDrawable boyAnimation;
-    //public AnimationDrawable boyAnimation2;
-    //public AnimationDrawable boyAnimation3;
-
     public ImageView robotImageView;
+
+    public AnimationDrawable boyAnimation;
     public AnimationDrawable robotAnimation;
 
     private SoundPool soundPool;
     private int sound07;
 
+    /******************************************
+     *   ON CREATE
+     ******************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,16 +55,19 @@ public class EndingActivity extends AppCompatActivity {
         height = displayMetrics.heightPixels;
         width = displayMetrics.widthPixels;
 
+        // Set Views
         congratView = (TextView) findViewById(R.id.congratView);
-        logoView = (ImageView) findViewById(R.id.logoView);
 
+        logoView = (ImageView) findViewById(R.id.logoView);
         bgImageView = (ImageView) findViewById(R.id.bgImageView);
+        boyImageView = (ImageView) findViewById(R.id.boyImageView);
+        robotImageView = (ImageView) findViewById(R.id.robotImageView);
+
+        //button set to invisible until credit sequence is finished
         mainMenu = (Button) findViewById(R.id.menuBTN);
         mainMenu.setVisibility(View.INVISIBLE);
-        boyImageView = (ImageView) findViewById(R.id.boyImageView);
-        //boyImageView2 = (ImageView) findViewById(R.id.boyImageView2);
-        //boyImageView3 = (ImageView) findViewById(R.id.boyImageView3);
-        robotImageView = (ImageView) findViewById(R.id.robotImageView);
+
+        // Get Game Level
         gameSettings = getSharedPreferences("GameSettings", Context.MODE_PRIVATE);
         editGame = gameSettings.edit();
         level = gameSettings.getInt("level", 0);
@@ -97,44 +98,55 @@ public class EndingActivity extends AppCompatActivity {
         // Here we load the mp3 files after the tools are set.
         sound07 = soundPool.load(this, R.raw.sound07, 1);
     }
-
+    /******************************************
+     *   EXIT GAME
+     *   Exit to main menu
+     ******************************************/
     public void exitGame(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
-        // SFX Implementation
+        // Victory Song SFX Implementation
         switch (view.getId()) {
             case R.id.menuBTN:
                 soundPool.play(sound07, 1, 1, 0, 0, 1);
                 break;
         }
     }
-
+    /******************************************
+     *   ON START
+     ******************************************/
     @Override
     protected void onStart() {
         super.onStart();
+
+        // Set Credits Animation
         congratView.setY((float) (height * 1));
         congratView.setX((float) (width * 0));
         congratView.animate().translationYBy((float)(-height * 2)).setDuration(20000);
 
+        // Set Logo Animation
         logoView.setY((float) (height * 2));
         logoView.setX((float) (width * 0));
         logoView.animate().translationYBy((float)(-height * 2)).setDuration(25000);
 
+        // Set Boy Animation
         boyImageView.setBackgroundResource(R.drawable.boyrun);
         boyAnimation = (AnimationDrawable) boyImageView.getBackground();
         boyImageView.setY((float) (height * .01));
         boyImageView.setX((float) (-width * 1));
+
+        // 1st Boy Run, from left to right
         boyAnimation.start();
         boyImageView.animate().translationXBy((float) (width * 2.3)).setDuration(10000);
 
+        // Set Robot Animation and 1st Robot Run, from left to right
         robotImageView.postDelayed(new Runnable() {
             @Override
             public void run() {
-                robotImageView.setScaleX(-1);
+                robotImageView.setScaleX(-1); //reverse image
                 robotImageView.setBackgroundResource(R.drawable.robotrun);
                 robotAnimation = (AnimationDrawable) robotImageView.getBackground();
-                //robotImageView.setY((float) (height * .5));
                 robotImageView.setY((float)(height*.56));
                 robotImageView.setX((float) (-width * 1));
                 robotAnimation.start();
@@ -142,67 +154,65 @@ public class EndingActivity extends AppCompatActivity {
             }
         }, 1000);
 
+        // 2nd Boy Run, from right to left
         boyImageView.postDelayed(new Runnable() {
             @Override
             public void run() {
-                boyImageView.setScaleX(-1);
+                boyImageView.setScaleX(-1); //reverse image
                 boyImageView.setBackgroundResource(R.drawable.boyrun);
                 boyAnimation = (AnimationDrawable) boyImageView.getBackground();
-                //boyImageView.setY((float) (height * .4));
                 boyImageView.setX((float) (width * 1));
                 boyAnimation.start();
                 boyImageView.animate().translationXBy((float) (-width * 2.5)).setDuration(10000);
             }
         }, 10000);
 
+        // 2nd Robot Run, from right to left
         robotImageView.postDelayed(new Runnable() {
             @Override
             public void run() {
-                robotImageView.setScaleX(1);
+                robotImageView.setScaleX(1); //set image to normal
                 robotImageView.setBackgroundResource(R.drawable.robotrun);
                 robotAnimation = (AnimationDrawable) robotImageView.getBackground();
-                //robotImageView.setY((float) (height * .5));
                 robotImageView.setX((float) (width * 1));
                 robotAnimation.start();
                 robotImageView.animate().translationXBy((float) (-width * 2)).setDuration(10000);
             }
         }, 11000);
 
+        // 3rd Boy Run, from left to idling in mid-screen
         boyImageView.postDelayed(new Runnable() {
             @Override
             public void run() {
-                boyImageView.setScaleX(1);
+                boyImageView.setScaleX(1); //set image to normal
                 boyImageView.setBackgroundResource(R.drawable.boyrun);
                 boyAnimation = (AnimationDrawable) boyImageView.getBackground();
-                //boyImageView.setY((float) (height * .4));
                 boyImageView.setX((float) (-width * 1));
                 boyAnimation.start();
                 boyImageView.animate().translationXBy((float) (width * 1.4)).setDuration(7000);
                     boyImageView.postDelayed(new Runnable() {
                     @Override
-                    public void run() {
+                    public void run() {  // boy idle animation
                             boyImageView.setBackgroundResource(R.drawable.boyidle);
                             boyAnimation = (AnimationDrawable) boyImageView.getBackground();
                             boyAnimation.start();
                             boyImageView.postDelayed(new Runnable() {
                             @Override
-                            public void run() {
-                                boyImageView.setScaleX(-1);
+                            public void run() { //boy fainting animation
+                                boyImageView.setScaleX(-1); //reverse image
                                 ViewGroup.LayoutParams params = (ViewGroup.LayoutParams) boyImageView.getLayoutParams();
-                                params.width *= 1.75;
-                                params.height *= 1.014;
+                                params.width *= 1.75;  //change width, since faint png is much wider
+                                params.height *= 1.014; //slightly change height, since faint png is a little taller
                                 boyImageView.setBackgroundResource(R.drawable.boyfaint);
                                 boyAnimation = (AnimationDrawable) boyImageView.getBackground();
                                 boyAnimation.start();
                                 boyImageView.animate().translationXBy((float) (-width * .1)).setDuration(700);
                                 boyImageView.postDelayed(new Runnable() {
                                     @Override
-                                    public void run() {
-                                        boyImageView.setScaleX(-1);
+                                    public void run() { //replace image with last faint frame
+                                        boyImageView.setScaleX(-1); //reverse image
                                         boyImageView.setBackgroundResource(R.drawable.boyfaint5);
-                                        //boyImageView3.animate().translationXBy((float) (width * 1.45)).setDuration(100);
-                                        //boyAnimation3.start();
-                                        mainMenu.setVisibility(View.VISIBLE);
+                                        mainMenu.setVisibility(View.VISIBLE); // make mainMenu button visible
                                     }
                                 }, 700);
                             }
@@ -212,25 +222,24 @@ public class EndingActivity extends AppCompatActivity {
             }
         }, 15000);
 
+        // 3rd Robot Run, from right to mid-screen
         robotImageView.postDelayed(new Runnable() {
             @Override
             public void run() {
-                //robotImageView.setScaleX(1);
                 robotImageView.setBackgroundResource(R.drawable.robotrun);
                 robotAnimation = (AnimationDrawable) robotImageView.getBackground();
-                //robotImageView.setY((float) (height * .5));
                 robotImageView.setX((float) (width * 1));
                 robotAnimation.start();
                 robotImageView.animate().translationXBy((float) (-width * .5)).setDuration(1500);
                 robotImageView.postDelayed(new Runnable() {
                     @Override
-                    public void run() {
+                    public void run() { //Robot Headbutt animation
                         robotImageView.setBackgroundResource(R.drawable.robotmelee);
                         robotAnimation = (AnimationDrawable) robotImageView.getBackground();
                         robotAnimation.start();
                         robotImageView.postDelayed(new Runnable() {
                             @Override
-                            public void run() {
+                            public void run() { //Robot Idle animation
                                 robotImageView.setBackgroundResource(R.drawable.robotidle);
                                 robotAnimation = (AnimationDrawable) robotImageView.getBackground();
                                 robotAnimation.start();
@@ -241,11 +250,14 @@ public class EndingActivity extends AppCompatActivity {
             }
         }, 22000);
     }
+    /******************************************
+     *   ON STOP
+     *   Stop animations
+     ******************************************/
     @Override
     protected void onStop() {
         super.onStop();
         boyAnimation.stop();
-        //monsterAnimation.stop();
         robotAnimation.stop();
     }
 }
